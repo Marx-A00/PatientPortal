224ed5198db7afe153a9f13b3ff0c758620335d4
york-marcos <MandradeC@yorksolutions.net>
2025-07-15 17:27:02 -0500

    wip: adding Patient to DB schema

---
+++ Models/Patient.cs
@@ -0,0 +1,10 @@
+namespace PatientPortal.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DOB { get; set; }
+    public string Email { get; set; }
+    
+}
