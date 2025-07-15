5b2205d5a56c8d954ec660922a6d8acb491604af
york-marcos <MandradeC@yorksolutions.net>
2025-07-15 17:28:09 -0500

    wip: made some fields required

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -3,8 +3,8 @@ namespace PatientPortal.Models;
 public class Patient
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime DOB { get; set; }
-    public string Email { get; set; }
+    public required string Email { get; set; }
     
 }
