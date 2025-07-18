c63d4affd6b13115d15439afed261aa2d7dcdd59
york-marcos <MandradeC@yorksolutions.net>
2025-07-18 13:41:04 -0500

    feat: Authentication working, able to sign in and sign out!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Okta.AspNetCore;
@@ -8,9 +8,7 @@ using PatientPortal.Data;
 using PatientPortal.Repositories;
 using PatientPortal.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Configuration.AddUserSecrets<Program>();
 
@@ -19,6 +17,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -31,35 +30,33 @@ builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-
-}).AddCookie()
+})
+.AddCookie()
 .AddOktaMvc(new OktaMvcOptions
 {
-        // Replace the Okta placeholders in appsettings.json with your Okta configuration.
     OktaDomain = builder.Configuration.GetValue<string>("Okta:OktaDomain"),
     ClientId = builder.Configuration.GetValue<string>("Okta:ClientId"),
     ClientSecret = builder.Configuration.GetValue<string>("Okta:ClientSecret"),
     AuthorizationServerId = builder.Configuration.GetValue<string>("Okta:AuthorizationServerId"),
-
 });
 
-builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
